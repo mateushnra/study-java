d536f703d26cdb3656bf562e12d58f23dd03c9af
Mateus <mateusandreassa@gmail.com>
2024-03-04 20:37:53 -0300

    Add update, delete and select method in project Agenda2024 GrupoDAO'

--- projetos/Agenda2024DB/src/Principal.java
+++ projetos/Agenda2024DB/src/Principal.java
@@ -4,13 +4,14 @@ import modelo.dao.GrupoDAO;
 public class Principal {
 	public static void main(String[] args) {
 		
-		Grupo grupo = new Grupo();
-		grupo.setCodigo(4);
-		grupo.setDescricao("Descrição do Grupo 4");
-		grupo.setNome("Grupo 4");
-		
 		
 		GrupoDAO grupoDAO = new GrupoDAO();
-		grupoDAO.inserir2(grupo);
+		Grupo grupo = grupoDAO.pesquisarPorCodigo(2);
+		if(grupo != null) {
+			System.out.println(grupo.getNome());
+			System.out.println(grupo.getDescricao());
+		}else {
+			System.out.println("Não há retorno para o grupo pesquisado!");
+		}
 	}
 }
